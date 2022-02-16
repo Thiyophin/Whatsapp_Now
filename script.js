7ba0e295d43f87905a3361ad17111f3d91a1d593
Thiyophin Johnson <thiyophinj99@gmail.com>
2022-02-16 11:35:55 +0530

    Alert box changed to sweetalert2

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ document.querySelector('#myForm').addEventListener('submit', function(e) {
   // console.log(typeof(number));
   // console.log(number.length);
   if(number.length!==10){
-    alert("Enter A Valid 10 Digit Number !!");
-    location.reload();
-    return;
+    // alert("Enter A Valid 10 Digit Number !!");
+    Swal.fire({
+      title: 'Not A Valid Number',
+      text: "Enter a valid 10 digit number !",
+      icon: 'error',
+      confirmButtonColor: '#ff0000',
+      confirmButtonText: 'Check Number'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        location.reload();
+         return;
+      }
+    })
+  }else{
+    window.location.replace("https://wa.me/91"+number);
   }
-  window.location.replace("https://wa.me/91"+number);
 });
