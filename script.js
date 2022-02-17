3a8fb26d25a97466b3241c239d7b9f1d4ac71855
Thiyophin Johnson <thiyophin.j@eyerov.com>
2022-02-17 17:20:09 +0530

    change

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 document.querySelector('#myForm').addEventListener('submit', function(e) {
   e.preventDefault()
   let number=this.elements.phoneNum.value;
-  // console.log(number);
-  // console.log(typeof(number));
-  // console.log(number.length);
   if(number.length!==10){
     // alert("Enter A Valid 10 Digit Number !!");
     Swal.fire({
